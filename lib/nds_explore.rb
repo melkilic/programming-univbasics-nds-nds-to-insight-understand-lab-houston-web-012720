cf5cc988f332251318d07307da09d8a9f6b6b30d
Melike KILIC <melike180400@gmail.com>
2020-01-21 03:30:53 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -2,7 +2,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 require 'pp'
-pp nds
 def pretty_print_nds(nds)
 pp nds
 end
