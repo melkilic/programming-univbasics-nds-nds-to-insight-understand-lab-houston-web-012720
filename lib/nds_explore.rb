9314c7508801c2d9f9211463952bc888dec271a6
Melike KILIC <melike180400@gmail.com>
2020-01-21 03:48:32 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,6 +9,8 @@ def print_first_directors_movie_titles
   movies=directors_database[0][:movies]
   index=0
   while index<movies.length do
-    
+    movie_titles= movies[:title]
+    puts movie_titles
+    index+=1
   end
 end
